f94d6dc6cdc1eba14649453967aac69f17cb919f
Thiago Ferreira <thiagofer2003@outlook.com>
2024-06-09 10:11:00 -0300

    LoginActivity e RegisterActivity funcionais

--- app/src/main/java/br/com/devhub/fragments/LoginFragment.java
+++ app/src/main/java/br/com/devhub/fragments/LoginFragment.java
@@ -1,19 +1,39 @@
 package br.com.devhub.fragments;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.Executor;
 
 import br.com.devhub.R;
+import br.com.devhub.activities.NavigationActivity;
 
 public class LoginFragment extends Fragment {
+
+    protected FirebaseAuth mAuth;
+    protected EditText edtEmail;
+    protected EditText edtPassword;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -37,8 +57,90 @@ public class LoginFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
-        // Botão para Cadastro
-        Button register = view.findViewById(R.id.register);
+        // Ação de Submit
+        onSubmit(view);
+
+        // Volta para Login Fragment
+        onRegisterFragment(view);
+
+        return view;
+    }
+
+
+    protected void onSubmit(View view) {
+        Button submit = view.findViewById(R.id.btnSubmit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtEmail = view.findViewById(R.id.edtEmail);
+                edtPassword = view.findViewById(R.id.edtPassword);
+
+                processLogin();
+            }
+        });
+    }
+
+
+    protected void processLogin() {
+        if (fieldsAreNotEmpty() && fieldsAreValid()) {
+
+            String email = String.valueOf(edtEmail.getText()).toLowerCase();
+            String password = String.valueOf(edtPassword.getText());
+
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FragmentManager fragmentManager = getParentFragmentManager();
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                            fragmentTransaction.replace(R.id.container, new HomeFragment());
+                            fragmentTransaction.commit();
+                        } else {
+                            Toast.makeText(getActivity(), "Login inválido... tente novamente!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        }
+    }
+
+
+    protected boolean fieldsAreNotEmpty() {
+        if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+            edtEmail.setError("Por favor, insira seu e-mail!");
+            edtEmail.requestFocus();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+            edtPassword.setError("Por favor, insira sua senha!");
+            edtPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+    protected boolean fieldsAreValid() {
+        if (!Patterns.EMAIL_ADDRESS.matcher(edtEmail.getText().toString()).matches()) {
+            edtEmail.setError("O e-mail deve ser válido!");
+            edtEmail.requestFocus();
+            return false;
+        }
+
+        if (edtPassword.getText().toString().length() < 8) {
+            edtPassword.setError("A senha deve conter pelo menos 8 caracteres!");
+            edtPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+
+    protected void onRegisterFragment(View view) {
+        Button register = view.findViewById(R.id.btnRegister);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,7 +151,5 @@ public class LoginFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-
-        return view;
     }
 }
--- app/src/main/java/br/com/devhub/fragments/RegisterFragment.java
+++ app/src/main/java/br/com/devhub/fragments/RegisterFragment.java
@@ -1,19 +1,42 @@
 package br.com.devhub.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.concurrent.Executor;
 
 import br.com.devhub.R;
+import br.com.devhub.activities.NavigationActivity;
 
 public class RegisterFragment extends Fragment {
+
+    protected FirebaseAuth mAuth;
+    protected EditText edtFullName;
+    protected EditText edtEmail;
+    protected EditText edtPassword;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -36,8 +59,86 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        // Botão para Login
-        Button login = view.findViewById(R.id.login);
+        // Ação de Submit
+        onSubmit(view);
+
+        // Volta para Login Fragment
+        onLoginFragment(view);
+
+        return view;
+    }
+
+
+    protected void onSubmit(View view) {
+        Button submit = view.findViewById(R.id.btnSubmit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtFullName = view.findViewById(R.id.edtFullname);
+                edtEmail = view.findViewById(R.id.edtEmail);
+                edtPassword = view.findViewById(R.id.edtPassword);
+
+                processNewUser();
+            }
+        });
+    }
+
+
+    protected void processNewUser() {
+        if (fieldsAreNotEmpty() && fieldsAreValid()) {
+
+            mAuth = FirebaseAuth.getInstance();
+
+            String fullname = edtFullName.getText().toString();
+            String email = edtEmail.getText().toString().toLowerCase();
+            String password = edtPassword.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            updateProfile(fullname);
+                        } else {
+                            String message;
+
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                message = "Já existe uma conta com este e-mail!";
+                            } else {
+                                message = "Parece que algo deu errado durante o seu cadastro. Pedimos que tente o envio novamente e se persistir o erro, entre em contato com o nosso suporte.";
+                            }
+
+                            Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+        }
+    }
+
+
+    protected void updateProfile(String fullname) {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(fullname)
+                .build();
+
+        user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    FragmentManager fragmentManager = getParentFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.container, new HomeFragment());
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+    }
+
+
+    protected void onLoginFragment(View view) {
+        Button login = view.findViewById(R.id.btnLogin);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +149,46 @@ public class RegisterFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+    }
 
-        return view;
+
+    protected boolean fieldsAreNotEmpty() {
+
+        if (TextUtils.isEmpty(edtFullName.getText().toString())) {
+            edtFullName.setError("Por favor, insira seu nome!");
+            edtFullName.requestFocus();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+            edtEmail.setError("Por favor, insira seu e-mail!");
+            edtEmail.requestFocus();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+            edtPassword.setError("Por favor, insira sua senha!");
+            edtPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+
+
+    protected boolean fieldsAreValid() {
+        if (!Patterns.EMAIL_ADDRESS.matcher(edtEmail.getText().toString()).matches()) {
+            edtEmail.setError("O e-mail deve ser válido!");
+            edtEmail.requestFocus();
+            return false;
+        }
+
+        if (edtPassword.getText().toString().length() < 8) {
+            edtPassword.setError("A senha deve conter pelo menos 8 caracteres!");
+            edtPassword.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
